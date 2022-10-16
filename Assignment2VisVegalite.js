818a796ded6aa936be308df140dfbb64f286c1b4
lachlan10176 <62224788+lachlan10176@users.noreply.github.com>
2022-10-17 00:42:09 +1100

    Layout mostly complete
    stand-in images, vis and text is present, needs to be replaced with chosen vis-es, idoms etc. Colour pallete in process. Meme temporary, but vital to sanity.

---
+++ Assignment2VisVegalite.js
@@ -0,0 +1,28 @@
+var vg_1 = "BarChart.json";
+vegaEmbed("#bar_chart", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "TopoMap.json";
+vegaEmbed("#map_one", vg_2).then(function(result) {}).catch(console.error);
+
+var vg_3 = "ChloroMap.json";
+vegaEmbed("#map_two", vg_3).then(function(result) {}).catch(console.error);
+
+var vg_4 = "BubblePlot.json";
+vegaEmbed("#bubble_one", vg_4).then(function(result) {}).catch(console.error);
+
+var vg_5 = "BubblePlot.json";
+vegaEmbed("#bubble_one_2", vg_5).then(function(result) {}).catch(console.error);
+
+var vg_6 = "BubblePlot.json";
+vegaEmbed("#bubble_one_3", vg_6).then(function(result) {}).catch(console.error);
+
+var vg_7 = "BubblePlot.json";
+vegaEmbed("#bubble_one_4", vg_7).then(function(result) {}).catch(console.error);
+
+var vg_8 = "BubblePlot.json";
+vegaEmbed("#bubble_one_5", vg_8).then(function(result) {}).catch(console.error);
+
+var vg_9 = "BubblePlot.json";
+vegaEmbed("#bubble_one_6", vg_9).then(function(result) {}).catch(console.error);
